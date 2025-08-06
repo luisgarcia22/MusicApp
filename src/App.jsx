ab88955769b11c9262d8eb80f69f8f2d76f25e52
LUISGARCIA <luis.garcia.arenas@ciisa.cl>
2025-08-05 20:47:43 -0400

    feat8 implemented responsive design. Closes #10

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,23 @@ import CurrentSong from './components/CurrentSong';
 import ActionButtons from './components/ActionButtons';
 import TrackList from './components/TrackList';
 import { tracks } from './data/tracks'; 
+import { useState,useEffect } from 'react';
 
 const App = () => {
+
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkScreen = () => setIsMobile(window.innerWidth < 768);
+    checkScreen();
+    window.addEventListener('resize', checkScreen);
+    return () => window.removeEventListener('resize', checkScreen);
+  }, []);
   return (
     <div className=" bg-black text-white">
       <CurrentSong />
       <div className="max-w-6xl mx-auto px-4 py-4">
-        <ActionButtons />
+        <ActionButtons isMobile={isMobile}/>
         <TrackList tracks={tracks} />
       </div>
     </div>
--- src/components/ActionButtons.jsx
+++ src/components/ActionButtons.jsx
@@ -2,8 +2,26 @@ import playButton from '../assets/Play Button.png';
 import followButton from '../assets/Follow-Button.png';
 import shuffleButton from '../assets/shuffle.png';
 
-export default function ActionButtons() {
-  return (
+
+export default function ActionButtons({ isMobile }) {
+    return isMobile ? (
+    <>
+      <div className="flex justify-between items-center mb-2">
+           <img
+          src={followButton}
+          alt="Play"
+          className="w-17 h-8 cursor-pointer"
+        />
+        <div className="flex gap-2 items-center">
+          <img src={shuffleButton} alt="Shuffle" className="w-6 h-6 cursor-pointer" />
+          <img src={playButton} alt="Play" className="w-8 h-8 cursor-pointer"/>
+        </div>
+      </div>
+      <div className="mb-2">
+        <h2 className="text-xl font-bold">Popular</h2>
+      </div>
+    </>
+  ) : (
     <div className="flex justify-between items-center w-full mb-4">
       <h2 className="text-xl font-bold">Popular</h2>
       <div className="flex items-center gap-[16px]">
